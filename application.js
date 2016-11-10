3c1d87962d78a76615585a724ef73207ff4892b2
Christina <christina.monets@gmail.com>
2016-11-10 16:37:59 +0200

    Add files via upload

---
+++ application.js
@@ -0,0 +1,16 @@
+var animals = [{
+  type: "Cat",
+  rating: 5,
+}, {
+  type: "Dog",
+  rating: 0,
+}, {
+  type: "Elephant",
+  rating 4,
+}, {
+  type: "Flea",
+  rating: 1
+}, {
+  type: "Hippo",
+  rating: 4
+}];
